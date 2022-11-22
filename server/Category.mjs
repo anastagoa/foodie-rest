8d112d4d76e0629503c9e448b06ba9250b013ee0
Asya <golubeva.stacia@gmail.com>
2022-11-22 13:27:33 +0300

    updated files

--- server/Category.mjs
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const Category = new mongoose.Schema({
-  title: {type: String, required: true},
-  id: {type: String, required: true},
-  dishes: {type: Array, required: true}
-})
-
-export default mongoose.model('Category', Category)
--- server/Promo.mjs
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const Promo = new mongoose.Schema({
-  title: {type: String, required: true},
-  description: {type: String, required: true},
-  img: {type: String, required: true}
-})
-
-export default mongoose.model('Promo', Promo)
